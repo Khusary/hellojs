a1190b01b0d95e3f8b75053704e06ff729a3d4da
Ibrahim Ningi <ibrahimdanladiningi3191@gmail.com>
2025-12-05 10:44:16 +0100

    intro file

---
+++ intro.js
@@ -0,0 +1,7 @@
+// const ibrahim = {
+//     name: "Ibrahim Danladi Ningi",
+//     age: 22,
+//     fav_prog_lang: "Javascript"
+// }
+
+console.log("My name is Ibrahim Danladi Ningi i am 22 years old, my favourite programming language is javascript ");
